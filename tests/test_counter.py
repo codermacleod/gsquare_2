7d98a0bb442946356b31871c88c6856a98f271b8
Daniel Macleod <codermacleod@gmail.com>
2022-11-10 12:32:56 +0000

    Made counter.py more modular by breaking each method into its own test

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -1,7 +1,17 @@
 from lib.counter import *
 
-def test_count_checker():
+def test_count_checker_reports_zero():
+    counter = Counter()
+    result = counter.report()
+    assert result == "Counted to 0 so far."
+
+def test_counter_adds_number():
     counter = Counter()
     counter.add(7)
-    result = counter.report()
-    assert result == "Counted to 7 so far."
+    assert counter.report() == "Counted to 7 so far."
+
+def test_counter_adds_multiple_numbers_to_count():
+    counter = Counter()
+    counter.add(5)
+    counter.add(2)
+    assert counter.report() == "Counted to 7 so far."
